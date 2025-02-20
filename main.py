bed05db3928b9decfe6264bb0b118857c78ad22a
Edward Trigos <edward.trigos@nivelics.co>
2025-02-19 22:34:35 -0500

    Add comment for feature branch and include new feature branch in main.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ if __name__ == "__main__" :
     print('Running...')
     print('Branch: feature/CONECTA-001') # comment in the feature line
     print('Hotfix in production') # add comment in the hotfix line
+    print('Branch: feature/CONECTA-002')
